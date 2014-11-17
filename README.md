ef1ab2fec3ef0343037b9ff43dd5b16ab53088cb
Bemi Faison <bemson@gmail.com>
2014-11-17 02:56:40 -0800

    Simplified event emitter

--- README.md
+++ README.md
@@ -260,24 +260,22 @@ This method simply exits if the bridge has already been initialized.
 Unsubscribe callback(s) from an event. When invoked with no arguments, all subscriptions are removed.
 
 ```
-SBridge.off([event [, callback [, scope]]]);
+SBridge.off([event [, callback ]]);
 ```
 
    * **event**: _(string)_ The event to unsubscribe. When omitted, all event subscribers are removed.
    * **callback**: _(function)_ The callback to detach from this event. When omitted, all callbacks are detached from this event.
-   * **scope**: _(object)_ Specifies detaching the given callback that is _also_ scoped to the given object. Do not use, unless you attached a callback with a particular scope.
 
 ### SBridge::on()
 
 Subscribe a callback to an event.
 
 ```
-SBridge.on(event, callback [, scope]);
+SBridge.on(event, callback);
 ```
 
    * **event**: _(string)_ An arbitrary event name.
    * **callback**: _(function)_ A callback to invoke when the event is fires.
-   * **scope**: _(object)_ An object to scope the callback invocation. By default, this is the EventEmitter instance.
 
 ### Sbridge::disabled
 
--- src/subetha-bridge.js
+++ src/subetha-bridge.js
@@ -611,6 +611,91 @@ SubEtha Message Bus (se-msg)
               msgHost('ready', origin);
             }
           }, Math.round(mathRandom() * 75));
+        },
+        on: function (evt, callback) {
+          var me = this;
+
+          if (
+            isFullString(evt) &&
+            typeof callback === 'function'
+          ) {
+            if (!protoHas.call(me, '_evts')) {
+              // init events hash
+              me._evts = {};
+            }
+            if (!protoHas.call(me._evts, evt)) {
+              // init event queue
+              me._evts[evt] = [];
+            }
+            // add callback to event queue
+            me._evts[evt].push(callback);
+          }
+          return me;
+        },
+        off: function (evt, callback) {
+          var
+            me = this,
+            cbs,
+            cbLn,
+            argLn = arguments.length;
+
+          if (!protoHas.call(me, '_evts') || !argLn) {
+            // reset if clearing all events
+            me._evts = {};
+          } else if (
+            isFullString(evt) &&
+            protoHas.call(me._evts, evt)
+          ) {
+            cbs = me._evts[evt];
+            if (typeof callback == 'function') {
+              cbLn = cbs.length;
+              // remove the last matching callback only
+              while (cbLn--) {
+                if (cbs[cbLn] === callback) {
+                  cbs.splice(cbLn, 1);
+                  break;
+                }
+              }
+            }
+            // remove event queue if no callback or none left
+            if (argLn < 2 || !cbs.length) {
+              delete me._evts[evt];
+            }
+          }
+
+          return me;
+        },
+        fire: function (evt) {
+          var
+            me = this,
+            params,
+            cbs,
+            cbLn,
+            cbIdx,
+            callbackInvoker;
+
+          if (
+            isFullString(evt) &&
+            protoHas.call(me, '_evts') &&
+            protoHas.call(me._evts, evt) &&
+            (cbLn = (cbs = me._evts[evt]).length)
+          ) {
+            params = protoSlice.call(arguments, 1);
+            if (params.length) {
+              callbackInvoker = function (cb) {
+                cb.apply(me, params);
+              };
+            } else {
+              callbackInvoker = function (cb) {
+                cb.call(me);
+              };
+            }
+            for (cbIdx = 0; cbIdx < cbLn; cbIdx++) {
+              callbackInvoker(cbs[cbIdx]);
+            }
+          }
+
+          return me;
         }
       }
     ;
@@ -1175,117 +1260,6 @@ SubEtha Message Bus (se-msg)
     function handleFirstPing(evt) {
       bridge.init(evt.data);
     }
-
-    // CLASSES
-
-    // basic event emitter
-    function EventEmitter() {}
-
-    mix(EventEmitter.prototype, {
-      on: function (evt, callback, scope) {
-        var me = this;
-
-        if (
-          isFullString(evt) &&
-          typeof callback === 'function'
-        ) {
-          if (!protoHas.call(me, '_evts')) {
-            me._evts = {};
-          }
-          if (!protoHas.call(me._evts, evt)) {
-            me._evts[evt] = [];
-          }
-          me._evts[evt].push({
-            cb: callback,
-            ctx: scope || me,
-            fctx: arguments.length > 2
-          });
-        }
-        return me;
-      },
-      off: function (evt, callback, scope) {
-        var
-          me = this,
-          subs,
-          keep,
-          sub,
-          subsIdx = 0,
-          noscope = arguments.length < 3,
-          remove;
-
-        if (!protoHas.call(me, '_evts') || !arguments.length) {
-          me._evts = {};
-        } else if (
-          isFullString(evt) &&
-          protoHas.call(me._evts, evt)
-        ) {
-          if (callback) {
-            subs = me._evts[evt];
-            keep = me._evts[evt] = [];
-            // determine which binds to keep
-            for (; sub = subs[subsIdx]; ++subsIdx) {
-              if (
-                // callbacks do not match
-                sub.cb !== callback ||
-                // no scope but one is specified
-                (noscope && sub.fctx) ||
-                // scopes do not match
-                sub.ctx !== scope
-              ) {
-                // keep this bind
-                keep.push(sub);
-              }
-            }
-            if (!keep.length) {
-              remove = 1;
-            }
-          } else {
-            remove = 1;
-          }
-          if (remove) {
-            delete me._evts[evt];
-          }
-        }
-
-        return me;
-      },
-      fire: function (evt) {
-        var
-          me = this,
-          params,
-          subs,
-          subsLn,
-          subsIdx,
-          invokeCallback;
-
-        if (
-          isFullString(evt) &&
-          protoHas.call(me, '_evts') &&
-          protoHas.call(me._evts, evt) &&
-          (subs = me._evts[evt]).length
-        ) {
-          params = protoSlice.call(arguments, 1);
-          if (params.length) {
-            invokeCallback = function (sub) {
-              sub.cb.apply(sub.ctx, params);
-            };
-          } else {
-            invokeCallback = function (sub) {
-              sub.cb.call(sub.ctx);
-            };
-          }
-          subsLn = subs.length;
-          for (subsIdx = 0; subsIdx < subsLn; ++subsIdx) {
-            invokeCallback(subs[subsIdx]);
-          }
-        }
-
-        return me;
-      }
-    });
-
-    // make module an event emitter
-    mix(bridge, EventEmitter.prototype);
 
     // exit now if platform is unsupported
     if (unsupported) {
